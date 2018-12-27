dc75c9a2c4d4d152c6660378b9b66472a14bb688
Tsungi Lyn <easy.lin@gmail.com>
2018-12-27 08:47:12 +0100

    prompt

--- .git-prompt-colors.sh
+++ .git-prompt-colors.sh
@@ -2,13 +2,17 @@ override_git_prompt_colors() {
 
   GIT_PROMPT_THEME_NAME="Custom"
 
+  Time12a="\$(date +%H:%M)"
+  AtHost=" @ "
+  AtPath=":"
   PathShort="\w"
   Username="\u"
   Hostname="\H"
 
-  GIT_PROMPT_START_USER="${Cyan}${Username}${White}@${Cyan}${Hostname}:${Yellow}${PathShort}${ResetColor}"
-  GIT_PROMPT_START_ROOT="${Red}${Username}${White}@${Cyan}${Hostname}:${Yellow}${PathShort}${ResetColor}"
-
+  GIT_PROMPT_START_USER="${Cyan}${Username}${Cyan}${AtHost}${Hostname}${AtPath}${Yellow}${PathShort}${ResetColor}"
+  GIT_PROMPT_START_ROOT="${Red}${Username}${Cyan}${AtHost}${Hostname}${AtPath}${Yellow}${PathShort}${ResetColor}"
+  GIT_PROMPT_END_USER=" \n${Yellow}${Time12a} $ ${ResetColor}"
+  GIT_PROMPT_END_ROOT=" \n${Yellow}${Time12a} # ${ResetColor}"
 }
 
 reload_git_prompt_colors "Custom"
