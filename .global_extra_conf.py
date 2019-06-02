f5d6c811bf2d0a16791b7d6163473900375e0aa3
easz <easy.lin@gmail.com>
2019-06-02 18:48:48 +0200

    basic flags

--- .global_extra_conf.py
+++ .global_extra_conf.py
@@ -1,97 +1,17 @@
-# Default global config for YouCompleteMe
-
-import os
-import ycm_core
-
-flags = [
-    '-x', 'c++',
-    '-Iinclude',
+def FlagsForFile( filename ):
+  return { 'flags': [
     '-Wall',
-    '-Werror',
     '-Wextra',
-    '-Wshadow',
-    '-pedantic',
-    '-std=c++17',
-]
+    '-Werror',
+    '-std=c++1z',
+    '-x', 'c++',
+    '-isystem', '/Library/Developer/CommandLineTools/usr/include/c++/v1',
+    '-isystem', '/usr/local/include',
+    '-isystem', '/Library/Developer/CommandLineTools/usr/lib/clang/10.0.0/include',
+    '-isystem', '/Library/Developer/CommandLineTools/usr/include',
+    '-isystem', '/usr/include',
+    '-isystem', '/System/Library/Frameworks',
+    '-isystem', '/Library/Frameworks',
+  ] }
 
-
-compilation_database_folder = ''
-
-if os.path.exists( compilation_database_folder ):
-  database = ycm_core.CompilationDatabase( compilation_database_folder )
-else:
-  database = None
-
-SOURCE_EXTENSIONS = [ '.C', '.cpp', '.cxx', '.cc', '.c', '.m', '.mm' ]
-
-def DirectoryOfThisScript():
-  return os.path.dirname( os.path.abspath( __file__ ) )
-
-
-def MakeRelativePathsInFlagsAbsolute( flags, working_directory ):
-  if not working_directory:
-    return list( flags )
-  new_flags = []
-  make_next_absolute = False
-  path_flags = [ '-isystem', '-I', '-iquote', '--sysroot=' ]
-  for flag in flags:
-    new_flag = flag
-
-    if make_next_absolute:
-      make_next_absolute = False
-      if not flag.startswith( '/' ):
-        new_flag = os.path.join( working_directory, flag )
-
-    for path_flag in path_flags:
-      if flag == path_flag:
-        make_next_absolute = True
-        break
-
-      if flag.startswith( path_flag ):
-        path = flag[ len( path_flag ): ]
-        new_flag = path_flag + os.path.join( working_directory, path )
-        break
-
-    if new_flag:
-      new_flags.append( new_flag )
-  return new_flags
-
-
-def IsHeaderFile( filename ):
-  extension = os.path.splitext( filename )[ 1 ]
-  return extension in [ '.H', '.h', '.hxx', '.hpp', '.hh' ]
-
-
-def GetCompilationInfoForFile( filename ):
-  if IsHeaderFile( filename ):
-    basename = os.path.splitext( filename )[ 0 ]
-    for extension in SOURCE_EXTENSIONS:
-      replacement_file = basename + extension
-      if os.path.exists( replacement_file ):
-        compilation_info = database.GetCompilationInfoForFile(
-          replacement_file )
-        if compilation_info.compiler_flags_:
-          return compilation_info
-    return None
-  return database.GetCompilationInfoForFile( filename )
-
-
-def FlagsForFile( filename, **kwargs ):
-  if database:
-    compilation_info = GetCompilationInfoForFile( filename )
-    if not compilation_info:
-      return None
-
-    final_flags = MakeRelativePathsInFlagsAbsolute(
-      compilation_info.compiler_flags_,
-      compilation_info.compiler_working_dir_ )
-
-  else:
-    relative_to = DirectoryOfThisScript()
-    final_flags = MakeRelativePathsInFlagsAbsolute( flags, relative_to )
-
-  return {
-    'flags': final_flags,
-    'do_cache': True
-  }
 
